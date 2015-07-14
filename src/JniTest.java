b1272705792aeefdf063ff2006a2c871bda6dddf
nautilus22 <nautilus22@gmail.com>
2015-07-14 16:46:40 +0900

    Initial commit

---
+++ src/JniTest.java
@@ -0,0 +1,16 @@
+public class JniTest {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+    static {
+        System.loadLibrary("TESTDLL");
+    }
+
+    public native void   printHelloWorld();
+    public native int    AddINT(int arg1, int arg2);
+    public native double addDBL(double arg1, double arg2);
+    public native String getStr(String str);
+}
